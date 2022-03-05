2212bff44db88062d997796a36a408aad3403bec
Carlos Biagolini-Jr <c.biagolini@gmail.com>
2022-03-04 21:25:43 -0300

    Base

---
+++ src/test/java/br/com/banco/dioBank/DioBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.banco.dioBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
